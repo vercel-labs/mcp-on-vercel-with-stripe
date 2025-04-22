2dd5c40809a73cbb5d848f302db1276d5cc9b549
Malte Ubl <cramforce@users.noreply.github.com>
2025-04-22 09:10:24 -0500

    Stateless MCP (#7)

--- lib/mcp-api-handler.ts
+++ lib/mcp-api-handler.ts
@@ -1,6 +1,7 @@
 import getRawBody from "raw-body";
 import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { SSEServerTransport } from "@modelcontextprotocol/sdk/server/sse.js";
+import { StreamableHTTPServerTransport } from "@modelcontextprotocol/sdk/server/streamableHttp.js";
 import { IncomingHttpHeaders, IncomingMessage, ServerResponse } from "http";
 import { createClient } from "redis";
 import { Socket } from "net";
@@ -42,13 +43,62 @@ export function initializeMcpApiHandler(
 
   let servers: McpServer[] = [];
 
+  let statelessServer: McpServer;
+  const statelessTransport = new StreamableHTTPServerTransport({
+    sessionIdGenerator: undefined,
+  });
+
   return async function mcpApiHandler(
     req: IncomingMessage,
     res: ServerResponse
   ) {
     await redisPromise;
     const url = new URL(req.url || "", "https://example.com");
-    if (url.pathname === "/sse") {
+    if (url.pathname === "/mcp") {
+      if (req.method === "GET") {
+        console.log("Received GET MCP request");
+        res.writeHead(405).end(
+          JSON.stringify({
+            jsonrpc: "2.0",
+            error: {
+              code: -32000,
+              message: "Method not allowed.",
+            },
+            id: null,
+          })
+        );
+        return;
+      }
+      if (req.method === "DELETE") {
+        console.log("Received DELETE MCP request");
+        res.writeHead(405).end(
+          JSON.stringify({
+            jsonrpc: "2.0",
+            error: {
+              code: -32000,
+              message: "Method not allowed.",
+            },
+            id: null,
+          })
+        );
+        return;
+      }
+      console.log("Got new MCP connection", req.url, req.method);
+
+      if (!statelessServer) {
+        statelessServer = new McpServer(
+          {
+            name: "mcp-typescript server on vercel",
+            version: "0.1.0",
+          },
+          serverOptions
+        );
+
+        initializeServer(statelessServer);
+        await statelessServer.connect(statelessTransport);
+      }
+      await statelessTransport.handleRequest(req, res);
+    } else if (url.pathname === "/sse") {
       console.log("Got new SSE connection");
 
       const transport = new SSEServerTransport("/message", res);
